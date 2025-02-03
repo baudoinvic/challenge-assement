06b596786870b80d0b389a49b529e696556cfa50
baudoin <baudoinvicbolingo@gmail.com>
2025-02-03 15:01:19 +0200

    fixing get column on crud

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -19,8 +19,8 @@ router.post("/customers", createCustomer);
 
 // PUT to update a customer
 router.put("/customers/:id", updateCustomer);
-
 // DELETE to remove a customer
 router.delete("/customers/:id", deleteCustomer);
+
 
 module.exports = router;
