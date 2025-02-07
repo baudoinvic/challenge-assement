68c8dcd833014035847f4697bed2653dbb890885
baudoin <baudoinvicbolingo@gmail.com>
2025-02-07 12:57:00 +0200

    fixed the index js file

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// const app = express();
-
-// // Import routes
-// const userRoutes = require("./routes/userRoutes");
-// const authRoutes = require("./routes/authRoutes");
-// const customerRoutes = require("./routes/customerRoutes");
-
-// // MongoDB connection string
-// const dbURI =
-//   "mongodb+srv://demo:12345@cluster0.5ukx8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// // Connect to MongoDB Atlas
-// mongoose
-//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log("Connected to MongoDB Atlas"))
-//   .catch((err) => console.log("Error connecting to MongoDB Atlas:", err));
-
-// app.use(express.json());
-
-// // Use routes
-// app.use("/api", customerRoutes)
-// app.use("/api", userRoutes);  
-// app.use("/api/auth", authRoutes);  
-// // Start server
-// const PORT = 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 
 const express = require("express");
 const mongoose = require("mongoose");
