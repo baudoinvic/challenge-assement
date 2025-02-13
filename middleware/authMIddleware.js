9466fcbe4157467713f87e32c612ca3f337a9c43
baudoin <baudoinvicbolingo@gmail.com>
2025-02-13 12:16:13 +0200

    midlware works correctly

--- middleware/authMIddleware.js
+++ middleware/authMIddleware.js
@@ -6,7 +6,7 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const verified = jwt.verify(token, "your_jwt_secret");
-    req.user = verified; // Attach user details to request
+    req.user = verified; 
     next();
   } catch (err) {
     res.status(400).json({ message: "Invalid Token" });
