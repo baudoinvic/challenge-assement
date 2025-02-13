23e2b5b0a2b86376526154e655eb24bedbd3ad49
baudoin <baudoinvicbolingo@gmail.com>
2025-02-13 12:18:56 +0200

    done with middlware

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,25 +25,6 @@ const registerUser = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
-// User login
-// const loginUser = async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-
-//     const passwordMatch = await bcrypt.compare(password, user.password);
-//     if (!passwordMatch)
-//       return res.status(400).json({ message: "Invalid credentials" });
-
-//     const token = jwt.sign({ userId: user._id }, "your_jwt_secret", {
-//       expiresIn: "1h",
-//     });
-//     res.json({ token });
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
 
    const loginUser = async (req, res) => {
      const { email, password } = req.body;
