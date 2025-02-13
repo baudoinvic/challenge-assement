a51ff81b59fa55a7bae91c28467d17ab484ea203
baudoin <baudoinvicbolingo@gmail.com>
2025-02-13 11:06:05 +0200

    working on it

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,24 +26,47 @@ const registerUser = async (req, res) => {
   }
 };
 // User login
-const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+// const loginUser = async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Invalid credentials" });
+//   try {
+//     const user = await User.findOne({ email });
+//     if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
-    const passwordMatch = await bcrypt.compare(password, user.password);
-    if (!passwordMatch)
-      return res.status(400).json({ message: "Invalid credentials" });
+//     const passwordMatch = await bcrypt.compare(password, user.password);
+//     if (!passwordMatch)
+//       return res.status(400).json({ message: "Invalid credentials" });
 
-    const token = jwt.sign({ userId: user._id }, "your_jwt_secret", {
-      expiresIn: "1h",
-    });
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+//     const token = jwt.sign({ userId: user._id }, "your_jwt_secret", {
+//       expiresIn: "1h",
+//     });
+//     res.json({ token });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+
+   const loginUser = async (req, res) => {
+     const { email, password } = req.body;
+
+     try {
+       const user = await User.findOne({ email });
+       if (!user)
+         return res.status(400).json({ message: "Invalid credentials" });
+
+       const passwordMatch = await bcrypt.compare(password, user.password);
+       if (!passwordMatch)
+         return res.status(400).json({ message: "Invalid credentials" });
+
+       const token = jwt.sign(
+         { userId: user._id, role: user.role }, // Include role in token
+         "your_jwt_secret",
+         { expiresIn: "1h" }
+       );
+
+       res.json({ token, role: user.role }); // Send role to frontend
+     } catch (err) {
+       res.status(500).json({ message: err.message });
+     }
+   };
 
 module.exports = { registerUser, loginUser };
