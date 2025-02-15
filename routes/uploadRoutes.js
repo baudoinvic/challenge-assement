4e4564a6ad74fe4fc4bc20f322088ca049f2e47c
baudoin <baudoinvicbolingo@gmail.com>
2025-02-15 18:34:34 +0200

    uploading file works

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,13 +1,20 @@
+
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const router = express.Router();
 
-// Configure multer storage
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir); 
+}
+
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); 
+    cb(null, uploadDir); 
   },
   filename: (req, file, cb) => {
     cb(
